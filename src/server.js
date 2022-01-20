9003726bd44a43eb346bf3a9172d6abff0c7feef
hansejun <tpwnszja2@gmail.com>
2022-01-20 15:31:46 +0900

    First Commit

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const PORT = 7070;
+
+app.listen(PORT, () =>
+  console.log(`✅ Server listening to ${PORT} port number! `)
+);
+
+const requeset = () => {};
