7e00be76eb8cc7513840526ba1835e0641608ce2
hansejun <tpwnszja2@gmail.com>
2022-01-21 16:21:31 +0900

    hashing password
    Password에 bcrypt를 사용하여 암호화를 하였다.

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,20 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String },
   email: { type: String, required: true, unique: true },
   name: { type: String, required: true },
+});
+
+// pre는 첫번째 파라미터로 설정된 event가 발생하기 전에 callback 함수를 실행시킨다.
+// "save" 이벤트는 Model.create / Model.save에 발생한다.
+userSchema.pre("save", async function () {
+  // isModified()는 해당값이 db에 기록된 값과 비교해서 변경된 경우 true, 그렇지 않은 경우 false를 반환한다.
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 5);
+  }
 });
 
 const User = mongoose.model("User", userSchema);
