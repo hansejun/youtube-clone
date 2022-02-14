25f44f485b1f404fec63b4da70f00444cc64ee57
hansejun <tpwnszja2@gmail.com>
2022-02-14 12:13:32 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -38,6 +38,12 @@ app.use(
     }),
   })
 );
+app.use((req, res, next) => {
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+});
+
 app.use(flash());
 app.use(localsMiddleware);
 app.use("/api", apiRouter);
