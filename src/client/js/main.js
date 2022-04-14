8226899b974217507146cdc114a4cb38c553c9ff
hansejun <tpwnszja2@gmail.com>
2022-04-14 16:53:27 +0900

    create search focus

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,33 +1,38 @@
 import "../scss/style.scss";
 import regeneratorRuntime from "regenerator-runtime";
 
-
-//message 
-const messageContainer = document.querySelector(".message")
+//message
+const messageContainer = document.querySelector(".message");
 const messageExitBtn = document.querySelector(".message-nav__exit");
-
+const searchFocusBtn = document.querySelector(".focus-search");
+const searchInput = document.querySelector(".header-nav__search input");
 const handleMessage = () => {
-    messageContainer.style.display = "none";
-}
+  messageContainer.style.display = "none";
+};
 
-if(messageContainer){
-    messageExitBtn.addEventListener("click",handleMessage);
+if (messageContainer) {
+  messageExitBtn.addEventListener("click", handleMessage);
 }
-
 
 // nav & list icon event
 const sidebarListIcon = document.querySelector(".header-list i");
 
 const handleIconEffect = (e) => {
-    const icon = e.target;
+  const icon = e.target;
+  icon.classList.toggle("backOpacity");
+  setTimeout(() => {
     icon.classList.toggle("backOpacity");
-    setTimeout(() => {
-        icon.classList.toggle("backOpacity");
-    }, 500);
+  }, 500);
+};
 
+const handleFocusSearch = (e) => {
+  e.preventDefault();
+  searchInput.focus();
+};
+
+if (sidebarListIcon) {
+  sidebarListIcon.addEventListener("click", handleIconEffect);
 }
-
-
-if(sidebarListIcon){
-    sidebarListIcon.addEventListener("click",handleIconEffect)
+if (searchFocusBtn) {
+  searchFocusBtn.addEventListener("click", handleFocusSearch);
 }
